75229383e1f40cb47c9a5b51e36c589cdf274284
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-18 10:48:19 +0900

    [ feat ] js practices : 객체

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,16 +1,43 @@
 /**
  * 변수와 자료형
  *
- * 기본타입
+ * [ 기본타입 ]
  * - undefined
  * - number
  * - string
  * - boolean
  *
- * 객체
+ * [ 객체 ]
  * - object
- * - function
+ *  1. new 생성자함수() 사용해서 생성
+ *  2. 리터럴
+ *      {}, [], //
+ *  3. null
  *
+ * - function
+ *  1. new Function() 사용해서 생성
+ *  2. 리터럴
+ *     var f = function(){...}
+ *  3. function f(){...}
+ *
+ */
+
+/**
+ * 자바스크립트 객체를 분류하는 또 다른 방법
+ *
+ * 1. 내장 객체
+ *    javascript engine 내부에 미리 내장(정의)되어 있는 객체
+ *    - 생성자 함수 : Number, Boolean, String, Date, ...
+ *
+ * 2. 호스트 객체
+ *    javascript 엔진이 embed 되어 있는 호스트 환경에 있는 객체
+ *    Brower 호스트: screen, location, navigator, document(DOM), XmlHttpRequest
+ *    Node 호스트: http, os, fs, path, process, server, ...
+ *
+ * 3. 사용자 객체
+ *    javascript 엔진이 실행되면서 생성하는 객체
+ *
+ * 3. 사용자 객체
  */
 
 console.log("=== 기본 타입(undefined, number, string, boolean) ===");
@@ -38,7 +65,6 @@ var F = function (a) {
 F(20);
 console.log(a, typeof F);
 
-
 var o = new F(10);
 console.log(o.a, typeof o);
 
@@ -50,12 +76,36 @@ var b2 = new Boolean(true);
 var o = new Object();
 var a = new Array();
 var d = new Date();
+var o2 = {};
+var a2 = [];
+var re2 = /^js$/gi;
+var n = null;
 
 console.log(i2 + ":" + typeof i2 + ":" + (i2 instanceof Number));
 console.log(b2 + ":" + typeof b2 + ":" + (b2 instanceof String));
 console.log(o + ":" + typeof o + ":" + (o instanceof Boolean));
 console.log(a + ":" + typeof a + ":" + (a instanceof Object));
 console.log(d + ":" + typeof d + ":" + (d instanceof Date));
+console.log(o2 + ":" + typeof o2 + ":" + (o2 instanceof String));
+console.log(a2 + ":" + typeof a2 + ":" + (a2 instanceof Boolean));
+console.log(re2 + ":" + typeof re2 + ":" + (re2 instanceof Object));
+console.log(n + ":" + typeof n + ":" + (n instanceof Date));
+
+console.log("=== 객체 타입(function)) ===");
+
+function f1(a, b) {
+  return a + b;
+}
+
+var f2 = function (a, b) {
+  return a + b;
+};
+
+var f3 = new Function("a", "b", "return a+b;");
+
+console.log(f1(10, 20) + ":" + typeof f1 + ":" + (f1 instanceof Function));
+console.log(f2(10, 20) + ":" + typeof f2 + ":" + (f2 instanceof Function));
+console.log(f3(10, 20) + ":" + typeof f3 + ":" + (f3 instanceof Function));
 
 // 유사 객체
 
